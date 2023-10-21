04b7e55527988863825a11f92942dbb185c7e1bf
Katia Bravo <katia@alexbravo.com>
2023-10-21 11:29:58 -0700

    syntax errors fixed

--- back/compare.js
+++ back/compare.js
@@ -1,34 +1,46 @@
 // for both student answer and wolfram answer
-const fs = require('fs');
-const student_latex = '/back/series_tester_incorrect.tex';
+// const fs = require('fs');
+import fs from "fs";
+// const student_latex = 'C:/Users/katia/Documents/GitHub/kadamb/back/series_tester_incorrect.tex';
+const student_latex = 'series_tester_incorrect.tex';
+// const wolfram_latex = 'C:/Users/katia/Documents/GitHub/kadamb/back/series_tester_correct.tex';
 const wolfram_latex = 'series_tester_correct.tex';
+
 // step 1: seperate lines into individual strings/objects (list)
 
-function splitLatexLines(latexPath) {
-    fs.readFile(latexPath, 'utf-8', (err, latexContent) => {
-        if (err) {
-            console.error('Error reading the file:', err);
-        } else {
-            return latexContent.split('\\\\'); // const filteredLatexArray = latexArray.filter(item => item.trim() !== '');
-        }
-    });
+async function splitLatexLines(latexPath) {
+    try{
+        console.log("latexPath", latexPath)
+        const data = fs.readFileSync(latexPath, 'utf-8', (err, latexContent) => {
+            // console.log("latexContent", latexContent)
+            // if (err) {
+            //     console.error('Error reading the file:', err);
+            // } else {
+            //     return latexContent.split(`\\`); // const filteredLatexArray = latexArray.filter(item => item.trim() !== '');
+            // }
+        });
+        // console.log(data.split(`\\\\`))
+        return data.split(`\\\\`);
+    } catch (err) {
+        console.log(err)
+    } 
 }
 
-student_pods = splitLatexLines(student_latex);
-wolfram_pods = splitLatexLines(wolfram_latex);
-
+var student_pods = await splitLatexLines(student_latex);
+var wolfram_pods = await splitLatexLines(wolfram_latex);
+// console.log(student_pods, wolfram_pods)
 // step 2: for each line, seperate the object into terms (list)
 
 function splitLatexTerms(pods) { //TODO: this should be looping through all the lines
-    split_pods = [pods.split('+')]
+    var split_pods = [pods.split('+')]
     // check if terms are fully split, if not, split using another character 
     // remove any empty items in the array before returning
     return split_pods;
 }
 
-student_broken = []
+var student_broken = []
 split_terms_in_lines(student_pods, student_broken);
-wolfram_broken = []
+var wolfram_broken = []
 split_terms_in_lines(wolfram_pods, wolfram_broken);
 
 function split_terms_in_lines(pods, broken) {
@@ -37,12 +49,23 @@ function split_terms_in_lines(pods, broken) {
     });
 }
 
-graded_work = []; // used to track term accuracy
+var graded_work = []; // used to track term accuracy
 
 // step 3: loop through items in wolfram, search the student answer for each term
 function check_student_work(s_broken, w_broken, compared_terms) {
     w_broken.forEach(function(w_term) {
-       compared_terms.push(s_broken.forEach(function(s_term) { // WARNING: may not loop through the two sets of terms correctly
+        const temp = s_broken.map(function(s_term) { // WARNING: may not loop through the two sets of terms correctly
+            // step 4: determine the accuracy of the answer
+            if (w_term == s_term) { // best case scenario
+                // TODO: right way to compare terms in latex?; seems to work for strings
+                return s_term; // TODO: possible this doesn't work
+            } else {
+                console.log("Wolfram term: ${w_term}, Student term: ${s_term}")
+                return "flag";
+            }
+        });
+        console.log("temp")
+       compared_terms.push(s_broken.map(function(s_term) { // WARNING: may not loop through the two sets of terms correctly
             // step 4: determine the accuracy of the answer
             if (w_term == s_term) { // best case scenario
                 // TODO: right way to compare terms in latex?; seems to work for strings
@@ -56,25 +79,26 @@ function check_student_work(s_broken, w_broken, compared_terms) {
     });
 }
 
-check_student_work(student_terms, wolfram_terms, graded_work); // used to track term accuracy
+check_student_work(student_broken, wolfram_broken, graded_work); // used to track term accuracy
 
 // step 5: summarize accuracy by calculating each line's term accuracy
 function accuracy_calculator(grades) {
-    total_accuracy = []
+    var total_accuracy = []
     grades.forEach(function(line_grade) {
-        flag_count = 0
+        var flag_count = 0
         line_grade.forEach(function(term) {
             if (term == "flag") flag_count++;
         });
-        line_accuracy = flag_count/line_grade.length;
+        var line_accuracy = flag_count/line_grade.length;
         total_accuracy.push(line_accuracy);
     });
     return total_accuracy;
 }
 
-student_grade = accuracy_calculator(graded_work);
+var student_grade = accuracy_calculator(graded_work);
 
 function print_grade(final_grade) {
+    console.log(final_grade)
     final_grade.forEach(function(step) {
         console.log(step);
     });
